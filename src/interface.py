72f9bb39bf5527c940ca5555e0b0031bc82b4118
Clinton Yeboah <clintonyeb@gmail.com>
2019-11-21 22:48:04 -0600

    implemented endpoints now efficiently

--- src/interface.py
+++ src/interface.py
@@ -17,6 +17,8 @@ class EntryPoint:
         self.path = None
         self.payload = None
         self.response = None
+        self.line_no = None
+        self.decorators = list()
 
 
 class ExitPoint:
@@ -124,9 +126,11 @@ def get_exit_points(file_name):
 
 
 def process_exit_points(project_path):
+    exit_points = list()
     for path, file_name in astor.code_to_ast.find_py_files(project_path):
         file_path = os.path.join(path, file_name)
-        return get_exit_points(file_path)
+        exit_points.extend(get_exit_points(file_path))
+    return exit_points
 
 
 def format_path(root_path, curr_file):
@@ -260,29 +264,111 @@ def get_object_at_import(root_package, import_, module_name):
     return None, module_path
 
 
+def get_view_at_import(root_package, module_name, level, func_name):
+    view_path = os.path.join(root_package, '.' * level + '/' + module_name + '.py')
+    view_path = os.path.normpath(view_path)
+    with open(view_path, "r") as source:
+        ast_node = astroid.parse(source.read())
+    return ast_node.lookup(func_name)[1][0], view_path
+
+
+def check_if_urlpatterns(node):
+    if node.is_statement:
+        targets = node.targets
+        if targets:
+            target = targets[0]
+            if hasattr(target, 'name') and target.name == 'urlpatterns':
+                return True
+    return False
+
+
+def get_url_statements(root):
+    for n in root.get_children():
+        if isinstance(n, astroid.nodes.Assign):
+            if check_if_urlpatterns(n):
+                yield n
+
+
+def process_file_for_url_patterns(root_file, file_name):
+    end_points = list()
+
+    with open(file_name, "r") as source:
+        ast_node = astroid.parse(source.read())
+        # use ilookup('urlpatterns')
+        for node in get_url_statements(ast_node):
+            # print(node.repr_tree())
+            for pattern in node.value.elts:
+                end_point = EntryPoint()
+                path = next(pattern.args[0].infer()).value
+                end_point.path = path
+                # end_point.func_name = pattern
+
+                name = None
+                if pattern.keywords:
+                    name = next(pattern.keywords[0].value.infer()).value
+
+                view = pattern.args[1]
+                add_node = False
+                if isinstance(view, astroid.Attribute):
+                    add_node = True
+                    if not name:
+                        name = view.attrname
+                    end_point.name = name
+
+                    view_el = view.expr.name
+                    imp = ast_node.lookup(view_el)[1][0]
+
+                    view_func, file_ = get_view_at_import(root_file, imp.names[0][0], imp.level, view.attrname)
+                    end_point.line_no = view_func.lineno
+                    end_point.name = view_func.name
+                    end_point.func_name = file_
+
+                    payload = dict()
+                    for arg in view_func.args.args:
+                        arg_name = arg.name
+                        payload[arg_name] = {}
+
+                    end_point.payload = payload
+
+                    response = {}
+                    end_point.response = response
+
+                    # Decorators
+                    if view_func.decorators:
+                        decs = view_func.decorators.nodes
+                        for n in decs:
+                            end_point.decorators.append(n.name)
+
+                if add_node:
+                    end_points.append(end_point)
+
+    return end_points
+
+
 def get_end_points(file_path):
-    # Find manage.py
-    manage = 'manage.py'
-    manage = os.path.join(file_path, manage)
-    with open(manage, "r") as source:
-        tree = ast.parse(source.read())
-    setting_path = get_project_settings(tree)
+    file_paths = os.path.join(file_path, '*.py')
+    end_points = list()
 
-    setting_path = format_path(file_path, setting_path)
-    with open(setting_path, "r") as source:
-        tree = ast.parse(source.read())
-    root_conf = get_root_conf(tree)
+    for file in glob.iglob(file_paths):
+        end_points.extend(process_file_for_url_patterns(file_path, file))
 
-    root_conf = format_path(file_path, root_conf)
-    with open(root_conf, "r") as source:
-        tree = ast.parse(source.read())
-    project_url_patterns = get_urls(tree, file_path)
+    return end_points
 
-    return project_url_patterns
-
-
-def find_py_files(src):
-    file_paths = os.path.join(src, '*.py')
-    for file in glob.glob(file_paths):
-        yield file
-
+    # # Find manage.py
+    # manage = 'manage.py'
+    # manage = os.path.join(file_path, manage)
+    # with open(manage, "r") as source:
+    #     tree = ast.parse(source.read())
+    # setting_path = get_project_settings(tree)
+    #
+    # setting_path = format_path(file_path, setting_path)
+    # with open(setting_path, "r") as source:
+    #     tree = ast.parse(source.read())
+    # root_conf = get_root_conf(tree)
+    #
+    # root_conf = format_path(file_path, root_conf)
+    # with open(root_conf, "r") as source:
+    #     tree = ast.parse(source.read())
+    # project_url_patterns = get_urls(tree, file_path)
+    #
+    # return project_url_patterns
